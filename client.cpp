5828e98fbfc1b76db9eab1fd56158654ebdbec8c
husseinkhaled733 <husseinkhaled733@gmail.com>
2023-12-04 12:59:44 +0200

    implementing client and testing

---
+++ client.cpp
@@ -0,0 +1,151 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Utility function to print error messages and exit
+void DieWithUserMessage(const string& msg, const string& detail) {
+    cerr << msg << ": " << detail << endl;
+    exit(1);
+}
+
+void DieWithSystemMessage(const string& msg) {
+    perror(msg.c_str());
+    exit(1);
+}
+
+// Split a string into tokens using a specified delimiter
+vector<string> split(const string& s, char delimiter) {
+    vector<string> tokens;
+    string token;
+    istringstream tokenStream(s);
+    while (getline(tokenStream, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+// Generate an HTTP request string
+string getRequest(const string& method, const string& filePath, const string& hostName, in_port_t portNumber) {
+    stringstream ss;
+    ss << method << " " << filePath << " HTTP/1.1\r\nHost: " << hostName << "\r\n\r\n";
+    return ss.str();
+}
+
+// Send an HTTP request over a socket
+void sendRequest(int sock, const string& request) {
+    ssize_t numBytes = send(sock, request.c_str(), request.length(), 0);
+    if (numBytes < 0) {
+        DieWithSystemMessage("send() failed");
+    } else if (numBytes != request.length()) {
+        DieWithUserMessage("send()", "sent unexpected number of bytes");
+    }
+}
+
+// Receive an HTTP response from a socket and write it to a file
+void receiveResponse(int sock, ofstream& outputFile) {
+    char buffer[1024];
+    while (true) {
+        ssize_t numBytes = recv(sock, buffer, sizeof(buffer) - 1, 0);
+        if (numBytes < 0) {
+            DieWithSystemMessage("recv() failed");
+        } else if (numBytes == 0) {
+            break;
+        }
+
+        buffer[numBytes] = '\0';
+        cout << buffer;
+        outputFile << buffer;
+    }
+}
+
+int main(int argc, char *argv[]) {
+   // Check command-line parameters
+   if (argc != 3) {
+       DieWithUserMessage("Parameter(s)", "<server-ip> <port-number>");
+   }
+
+   // Set server IP and port number
+   const char *serverIP = argv[1];
+   in_port_t portNumber = atoi(argv[2]);
+
+   // Create a socket
+   int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+   if (sock < 0) {
+       DieWithSystemMessage("socket() failed");
+   }
+
+   // Configure the socket
+   sockaddr_in servAddr;
+   memset(&servAddr, 0, sizeof(servAddr));
+   servAddr.sin_family = AF_INET;
+
+   // Convert the IP address from a string to a numeric value
+   int rtnVal = inet_pton(AF_INET, serverIP, &servAddr.sin_addr.s_addr);
+   if (rtnVal == 0) {
+       DieWithUserMessage("inet_pton() failed", "invalid address string");
+   } else if (rtnVal < 0) {
+       DieWithSystemMessage("inet_pton() failed");
+   }
+
+   // Set the port number
+   servAddr.sin_port = htons(portNumber);
+
+   // Connect to the server
+   if (connect(sock, (sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
+       DieWithSystemMessage("connect() failed");
+   }
+
+   // Open the input file
+   ifstream file("input.txt");
+   if (!file.is_open()) {
+       DieWithUserMessage("File not found", "input.txt");
+   }
+
+   // Process each line in the input file
+   string line;
+   while (getline(file, line)) {
+       vector<string> args = split(line, ' ');
+       if (args[0] == "GET" || args[0] == "POST") {
+           const char *filePath = args[1].c_str();
+           const char *hostName = args[2].c_str();
+           in_port_t portNumber = (args.size() == 4) ? atoi(args[3].c_str()) : 80;
+
+           // Send the request
+           string request = getRequest(args[0], filePath, hostName, portNumber);
+           sendRequest(sock, request);
+
+           // Receive and save the response
+           cout << "Received: ";
+
+           string Path="./clientfiles";
+           Path.append(filePath);
+           ofstream outputFile(filePath);
+           if (!outputFile.is_open()) {
+               DieWithUserMessage("Failed to create file", filePath);
+           }
+
+           receiveResponse(sock, outputFile);
+
+           cout << endl;
+           outputFile.close();
+       } else {
+           DieWithUserMessage("Unsupported command", args[0]);
+       }
+   }
+
+   // Close the file and socket
+   file.close();
+   close(sock);
+   exit(0);
+}
